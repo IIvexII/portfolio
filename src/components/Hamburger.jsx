d2cd6d15f974b4454bed7a011276fa74753ea9e4
Zafeer Hafeez <contact@zafeer.pk>
2024-03-16 16:09:18 +0500

    feat: add group hover description in hamburger

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -14,7 +14,7 @@ export default function Hamburger({ pageInfo }) {
 
       {/* menu to show when button clicked */}
       <div
-        className={`absolute left-[-19px] mt-2 bg-white border border-gray-800 shadow-md rounded-full transition duration-300 ease-in-out bg-opacity-95 ${
+        className={`absolute -translate-x-3 mt-2 border-b-0 border-t border-gray-600 bg-white shadow-md rounded-full transition duration-500 ease-in-out bg-opacity-95 ${
           showMenu ? "opacity-100 visible" : "opacity-0 invisible"
         }`}
       >
@@ -23,21 +23,28 @@ export default function Hamburger({ pageInfo }) {
             <a
               key={index}
               href={page.link}
-              className={`w-full flex flex-col justify-center items-center space-y-1 py-3 px-3 text-lg transition-all duration-300 hover:bg-black hover:text-white ${
+              className={`group relative w-full py-3 px-3 text-lg transition-all duration-300 hover:bg-black hover:text-white ${
                 index === 0 ? "pt-6 rounded-t-full" : ""
               }`}
             >
               <FontAwesomeIcon icon={page.icon} className="w-5" />
-              <p className="text-xs">{page.name}</p>
+
+              <p className="group-hover:opacity-100 group-hover:visible opacity-0 invisible absolute translate-x-11 -translate-y-6 text-xs text-black bg-white px-3 py-1 border border-black rounded transition duration-700 ease-in-out">
+                {page.name}
+              </p>
             </a>
           ))}
 
           {/* Close Button */}
           <a
             onClick={() => setShowMenu(!setShowMenu)}
-            className="w-full flex justify-center py-5 px-3 text-lg text-white bg-black rounded-b-full transition-all duration-300"
+            className="relative group w-full flex justify-center py-5 px-3 text-lg text-white bg-black  hover:bg-red-700 rounded-b-full transition-all duration-300"
           >
             <FontAwesomeIcon icon={faXmark} className="w-5" />
+
+            <p className="group-hover:opacity-100 group-hover:visible opacity-0 invisible  absolute translate-x-14 text-xs text-black bg-white px-3 py-1 border border-black rounded transition duration-700 ease-in-out">
+              Close
+            </p>
           </a>
         </div>
       </div>
