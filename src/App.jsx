ca71e963aeb4df039496d2fd584016a077e1d61a
Zafeer Hafeez <contact@zafeer.pk>
2024-03-16 17:32:43 +0500

    feat: add small description when hover to nav links

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,29 @@ export default function App() {
   return (
     <>
       <Header name="zafeer" socialInfo={socialInfo} pageInfo={pageInfo} />
-      <main>
+      <main className="snap-y snap-mandatory h-screen overflow-auto scroll-smooth">
         {/* Hero Section */}
         <HeroSection profileImage={profileImage} quote={quotes[0]} />
+
+        {/* About me section */}
+        <section className="snap-start flex justify-center bg-black text-white h-screen min-h-[630px] md:max-h-[660px]">
+          <div className="px-8 h-full w-full flex justify-center items-center max-w-[1024px] max-sm:flex-col-reverse max-sm:space-y-20 max-sm:pb-16">
+            <article>
+              <h1 className="text-4xl">About Me</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                vitae sapien id dolor fermentum tincidunt. Nullam auctor, nunc
+                nec fermentum tincidunt, nunc mi tincidunt orci, eget luctus
+                odio libero vel turpis
+              </p>
+            </article>
+            <img
+              src={profileImage}
+              alt="My Image"
+              className="w-[500px] max-sm:w-[300px] select-none pointer-events-none"
+            />
+          </div>
+        </section>
       </main>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { DropletDropdown, Hamburger } from "../components";
 
 export default function Header({ name, socialInfo, pageInfo }) {
   return (
-    <header className="fixed z-10 py-3 px-8 pb-4 w-full flex justify-center items-center bg-white bg-opacity-95 firefox:bg-opacity-95 border border-gray-200">
+    <header className="fixed z-10 py-3 px-8 pb-4 w-full flex justify-center items-center bg-white bg-opacity-95 firefox:bg-opacity-95">
       <div className="w-full flex justify-between items-center max-w-[1024px]">
         {/* Navigation Hamburger for mobiles */}
         <Hamburger pageInfo={pageInfo} />
@@ -17,15 +17,16 @@ export default function Header({ name, socialInfo, pageInfo }) {
         {/* Navigation icons */}
         <nav className="flex items-center px-8 space-x-8 text-xl max-sm:hidden">
           {pageInfo.map((page, index) => (
-            <abbr title={page.name} key={index}>
-              <a
-                href={page.link}
-                key={index}
-                className="flex flex-col justify-center items-center py-3 px-3 hover:bg-black hover:text-white rounded-full transition-all duration-300"
-              >
-                <FontAwesomeIcon icon={page.icon} className="w-5" />
-              </a>
-            </abbr>
+            <a
+              href={page.link}
+              key={index}
+              className="group relative flex flex-col justify-center items-center py-3 px-3 hover:bg-black hover:text-white rounded-full transition-all duration-300"
+            >
+              <FontAwesomeIcon icon={page.icon} className="w-5" />
+              <p className="group-hover:opacity-100 group-hover:visible opacity-0 invisible absolute translate-y-10 text-xs text-black bg-white px-3 py-1 border border-gray-300 rounded transition duration-300 ease-in-out">
+                {page.name}
+              </p>
+            </a>
           ))}
         </nav>
 
